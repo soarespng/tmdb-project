dee1e5f8352247b55cd0929e035c0d7ac170d4af
CLUDE\guilherme.castro <guilherme.castro@clude.com.br>
2024-06-17 17:39:31 -0300

    add swiper for similar films

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,51 +1,76 @@
-import { useState, useEffect } from "react"
-import { useParams } from "react-router-dom"
-import { BsHourglassSplit } from "react-icons/bs"
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { BsHourglassSplit } from "react-icons/bs";
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-import MovieCard from "../components/MovieCard"
-import "./Movie.css"
+import 'swiper/css';
+import MovieCard from "../components/MovieCard";
+import "./Movie.css";
 
+const imageUrl = import.meta.env.VITE_IMG;
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
 const Movie = () => {
-    const {id} = useParams()
-    const [movie, setMovie] = useState(null)
+    const { id } = useParams();
+    const [movie, setMovie] = useState(null);
+    const [similar, setSimilar] = useState([]);
 
-    const getMovie = async(url) => {
+    const getMovie = async (url) => {
         const response = await fetch(url);
         const data = await response.json();
-
         setMovie(data);
-    }
+    };
+    
+    const getSimilar = async (url) => {
+        const response = await fetch(url);
+        const data = await response.json();
+        setSimilar(data.results);
+    };
 
     useEffect(() => {
-        const movieURL = `${moviesURL}${id}?${apiKey}&language=pt-BR`
-        getMovie(movieURL)
-    }, [id])
+        const movieURL = `${moviesURL}${id}?${apiKey}&language=pt-BR`;
+        getMovie(movieURL);
+    }, [id]);
+
+    useEffect(() => {
+        const similarURL = `${moviesURL}${id}/similar?${apiKey}&language=pt-BR`;
+        getSimilar(similarURL);
+    }, [id]);
 
     return (
         <div className="movie-container">
             {movie && (
                 <>
                     <div className="movie-header">
-                        <MovieCard movie={movie} showLink={false} />
+                        <img src={imageUrl + movie.poster_path} alt={movie.title} />
                         <div className="movie-info">
                             <h1 className="movie-title">{movie.title}</h1>
                             <p className="movie-tagline">{movie.tagline}</p>
                             <p className="movie-runtime">
                                 <BsHourglassSplit /> {movie.runtime} minutos
                             </p>
+                            <br/>
+                            <h2 className="movie-title">Sinopse</h2>
+                            <p>{movie.overview}</p>
                         </div>
                     </div>
-                    <div className="movie-overview">
-                        <h2>Sinopse</h2>
-                        <p>{movie.overview}</p>
+                    <div className="similar-movies">
+                        <h2 className="movie-title">Titulos similares:</h2>
+                        {similar.length > 0 && (
+                            <Swiper spaceBetween={5} slidesPerView={5}>
+                            {similar.map((similarMovie) => (
+                                <SwiperSlide key={similarMovie.id}>
+                                <MovieCard movie={similarMovie} />
+                                </SwiperSlide>
+                            ))}
+                            </Swiper>
+                        )}
                     </div>
                 </>
             )}
         </div>
-    )
-}
+    );
+};
 
-export default Movie
+export default Movie;
